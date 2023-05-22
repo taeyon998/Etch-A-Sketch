a0aa18700658f261af3334dcc549dc7efd07a4e0
Taeyon Kim <taeyon998@gmail.com>
2023-05-22 06:38:08 +0000

    rough draft 2

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+const GRIDWIDTH = 900;
+
 const container = document.querySelector('#container');
 const resetButton = document.getElementById('resetButton');
 
+// OUTPUT: rgb(n,n,n)
+function getRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
 function handleSquareHover(event) {
-    const square = event.target;
-    square.classList.add('hovered');
+    event.preventDefault(); // Prevent default dragging behavior
+
+    if (event.buttons === 1) { // Only when left mouse is clicked
+        const square = event.target;
+
+        // Opacity
+        let opacity = parseFloat(square.style.opacity) || 0; // parseFloat('0.1') = 0.1, parseFloat('Hello') = NaN
+        opacity += 0.2;
+        square.style.opacity = opacity;
+
+        // Color
+        if (opacity <= 0.2) {
+            square.style.backgroundColor = getRandomColor(); // rgb(255,0,0)
+        }
+        // square.style.backgroundColor = 'black';
+
+        // square.classList.add('hovered');
+    }
 }
 
 // function handleSquareLeave(event) {
@@ -23,11 +49,11 @@ function resetGrid() {
         container.firstChild.remove();
     }
 
-    container.style.width = `${960}px`;
+    container.style.width = `${GRIDWIDTH}px`;
 
     const borderSize = 2; // Default border=1 left & right within cell
     const totalborderSize = gridSize * borderSize; // Total size occupied by gaps
-    const sideLength = Math.floor((960 - totalborderSize) / gridSize); // Calculate the size of each grid cell
+    const sideLength = Math.floor((GRIDWIDTH - totalborderSize) / gridSize); // Calculate the size of each grid cell
 
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
@@ -37,6 +63,7 @@ function resetGrid() {
             square.style.height = `${sideLength}px`;
             container.appendChild(square);
 
+            square.addEventListener('mousedown', handleSquareHover);
             square.addEventListener('mouseover', handleSquareHover);
             // square.addEventListener('mouseleave', handleSquareLeave);
         }
